dffb9f7e7a2573c92730d1562b39e32ff5decc77
Ryan <salvat36@gmail.com>
2023-04-14 15:15:00 -0500

    Passing Lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+// findMatching- This function takes an array of drivers' names and a string as arguments, and returns the matching list of drivers. 
+// The function should be case insensitive.
+
+function findMatching(drivers, name) {
+    const driverName = drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+    return driverName;
+};
+findMatching(["Harry", "Joe", "Steve"], "joe");
+
+function fuzzyMatch(drivers, name) {
+    const driverName = drivers.filter(driver => driver.slice(0, name.length) === name);
+    return driverName;
+};
+fuzzyMatch(["Harry", "Joe", "Steve"], "Jo");
+
+function matchName(drivers, key) {
+    const driverName = drivers.filter(driver => driver.name === key);
+    return driverName;
+};
+matchName();
